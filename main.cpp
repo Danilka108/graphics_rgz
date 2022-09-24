3bcd5bc71854af2e7968867ad90fc1461e895329
danilka108 <danilka108@yandex.ru>
2022-09-25 01:17:08 +0700

    refactor and optimize code; update zooming

--- main.cpp
+++ main.cpp
@@ -1,116 +1,82 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <iostream>
 #include <math.h>
 
+#define FloatType double
+
 struct Complex {
-  double x;
-  double y;
+  FloatType real;
+  FloatType imag;
 };
 
-void resize(int width, int height) {}
-
-const int ITER_NUMBER = 50;
+const int DEFAULT_ITERS_OF_FORUMA_CALC = 100;
+const int MAX_COMPLEX_NUMBER_ABSOLUTE_VALUE = 16;
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
-double scale = 1;
-double x_correction_factor = 0;
-double y_correction_factor = 0;
+const int RENDER_QUARTER_WIDTH = 200;
+const int RENDER_QUARTER_HEIGHT = 200;
 
-int prev_mouse_x = NULL;
-int prev_mouse_y = NULL;
+const FloatType RENDER_STEP = 1;
+const FloatType ZOOM_STEP = 0.1;
 
-bool is_pressed = false;
+const int DEFAULT_ZOOM = 4;
 
-void mouse(int button, int state, int x, int y) {
-  is_pressed = state == GLUT_LEFT_BUTTON && state == GLUT_DOWN;
+void init();
+void onMouseStateChangeListener(int button, int state, int x, int y);
+void onMouseMotionListener(int x, int y);
+void onMouseEntryListener(int state);
+void onResizeListener(int width, int height);
+void onDisplayListener();
+void renderAxes();
+void renderFrame();
+void updateItersOfFormulaCalc();
+int calcProximityDegreeToCenter(FloatType x, FloatType y);
+bool isAllowableComplexValue(Complex number);
+void calcIterableFormula(Complex &z, Complex &c);
+void renderPoint(int n, int x, int y);
 
-  if (button == 3) {
-    scale += 1;
-  }
+FloatType zoom = DEFAULT_ZOOM;
 
-  if(button == 4) {
-    scale -= 1;
-  }
+FloatType scaleFactor = 1 / exp(DEFAULT_ZOOM);
 
-  prev_mouse_y = y;
-  prev_mouse_x = x;
+FloatType xShiftFactor = 0;
+FloatType yShiftFactor = 0;
 
-}
+int prevMouseXPos = NULL;
+int prevMouseYPos = NULL;
 
-void mouseMotion(int x, int y) {
-  if (prev_mouse_y == NULL || prev_mouse_x == NULL || !is_pressed) {
-    prev_mouse_y = y;
-    prev_mouse_x = x;
-    return;
-  }
-  x_correction_factor -= x - prev_mouse_x;
-  y_correction_factor += y - prev_mouse_y;
+bool doRender = true;
 
-  prev_mouse_y = y;
-  prev_mouse_x = x;
-}
+bool isMouseLeftBtnPressed = false;
 
-void mouseEntry(int state) {
-  if (state == GLUT_LEFT) {
-    is_pressed = false;
-  }
+int itersOfFormulaCalc = DEFAULT_ITERS_OF_FORUMA_CALC;
 
-  if (state == GLUT_ENTERED) {
-    is_pressed = true;
-  }
-}
+int main(int argc, char **argv) {
+  glutInit(&argc, argv);
 
-void display() {
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowPosition(50, 10);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-  glViewport(0, 0, 800, 800);
+  glutCreateWindow("Graphics Rgz");
 
-  glPointSize(1);
+  glutMouseFunc(onMouseStateChangeListener);
+  glutMotionFunc(onMouseMotionListener);
+  glutEntryFunc(onMouseEntryListener);
 
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glutReshapeFunc(onResizeListener);
+  glutDisplayFunc(onDisplayListener);
+  glutPostRedisplay();
+  glutIdleFunc(onDisplayListener);
 
-  glBegin(GL_POINTS);
+  init();
 
-  for (double y = -WINDOW_HEIGHT / scale / 2; y < WINDOW_HEIGHT / scale / 2;
-       y += 1 / scale) {
-    for (double x = -WINDOW_WIDTH / scale / 2; x < WINDOW_WIDTH / scale / 2;
-         x += 1 / scale) {
-      Complex z, z1, c, c1;
-
-      z.x = (x + x_correction_factor / scale) * 0.01;
-      z.y = (y + y_correction_factor / scale) * 0.01;
-      c.x = z.x;
-      c.y = z.y;
-
-      int max = 16;
-
-      int n = 0;
-      while (((z.x * z.x + z.y * z.y) < max) && (n < ITER_NUMBER)) {
-        z1 = z;
-        c1 = c;
-        z.x = (z1.x * z1.x) - (z1.y * z1.y) + c.x;
-        z.y = 2 * z1.x * z1.y + c.y;
-        c.x = c1.x / 2 + z.x;
-        c.y = c1.y / 2 + z.y;
-        n++;
-      }
-
-      if (n < ITER_NUMBER) {
-        double a = 35 * n % 255;
-        double b = 30 * n % 255;
-        double c = 25 * n % 255;
-        glColor3d(a / 255.0, b / 255.0, c / 255.0);
-        glVertex2d(x * scale, y * scale);
-      }
-    }
-  }
-
-  glEnd();
-
-  glFlush();
+  glutMainLoop();
+  return 0;
 }
 
 void init() {
@@ -122,25 +88,171 @@ void init() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(-WINDOW_WIDTH / 2, WINDOW_WIDTH / 2, -WINDOW_HEIGHT / 2,
-          WINDOW_HEIGHT / 2, 0, 2);
+  glOrtho(-RENDER_QUARTER_WIDTH, RENDER_QUARTER_WIDTH, -RENDER_QUARTER_HEIGHT,
+          RENDER_QUARTER_HEIGHT, 0, 2);
   gluLookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
   glMatrixMode(GL_MODELVIEW);
 }
 
-int main(int argc, char **argv) {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowPosition(50, 10);
-  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-  glutCreateWindow("Graphics Rgz");
-  glutReshapeFunc(resize);
-  glutMouseFunc(mouse);
-  glutMotionFunc(mouseMotion);
-  init();
-  glutDisplayFunc(display);
-  glutEntryFunc(mouseEntry);
-  glutIdleFunc(display);
-  glutMainLoop();
-  return 0;
+void onMouseStateChangeListener(int button, int state, int x, int y) {
+  isMouseLeftBtnPressed = state == GLUT_LEFT_BUTTON && state == GLUT_DOWN;
+
+  if (button == 3) {
+    zoom += ZOOM_STEP;
+  }
+
+  if (button == 4) {
+    zoom = zoom - ZOOM_STEP < DEFAULT_ZOOM ? DEFAULT_ZOOM : zoom - ZOOM_STEP;
+  }
+
+  if (button == 3 || button == 4) {
+    doRender = true;
+    scaleFactor = 1 / exp(zoom);
+  }
+
+  prevMouseYPos = y;
+  prevMouseXPos = x;
+}
+
+void onMouseMotionListener(int x, int y) {
+  if (prevMouseYPos == NULL || prevMouseXPos == NULL ||
+      !isMouseLeftBtnPressed) {
+    prevMouseYPos = y;
+    prevMouseXPos = x;
+
+    return;
+  }
+
+  if (prevMouseXPos != x || prevMouseYPos != y)
+    doRender = true;
+
+  xShiftFactor -= (FloatType)(x - prevMouseXPos) * scaleFactor;
+  yShiftFactor += (FloatType)(y - prevMouseYPos) * scaleFactor;
+
+  prevMouseYPos = y;
+  prevMouseXPos = x;
+}
+
+void onMouseEntryListener(int state) {
+  if (state == GLUT_LEFT) {
+    isMouseLeftBtnPressed = false;
+  }
+
+  if (state == GLUT_ENTERED) {
+    isMouseLeftBtnPressed = true;
+  }
+}
+
+void onResizeListener(int width, int height) {}
+
+void onDisplayListener() {
+  if (!doRender) {
+    glFlush();
+    return;
+  }
+
+  doRender = false;
+
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+  glPointSize(1);
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  renderAxes();
+  renderFrame();
+
+  glFlush();
+}
+
+void renderAxes() {
+  glBegin(GL_LINES);
+
+  glColor3d(1, 1, 1);
+
+  glVertex2d(0, RENDER_QUARTER_HEIGHT);
+  glVertex2d(0, -RENDER_QUARTER_HEIGHT);
+
+  glVertex2d(RENDER_QUARTER_WIDTH, 0);
+  glVertex2d(-RENDER_QUARTER_WIDTH, 0);
+
+  glEnd();
+}
+
+void renderFrame() {
+  updateItersOfFormulaCalc();
+
+  //std::cout << "\n" << itersOfFormulaCalc;
+
+  glBegin(GL_POINTS);
+
+  for (FloatType x = -RENDER_QUARTER_WIDTH; x < RENDER_QUARTER_WIDTH;
+       x += RENDER_STEP) {
+    for (FloatType y = -RENDER_QUARTER_HEIGHT; y < RENDER_QUARTER_HEIGHT;
+         y += RENDER_STEP) {
+      int degree = calcProximityDegreeToCenter(x * scaleFactor + xShiftFactor,
+                                               y * scaleFactor + yShiftFactor);
+      renderPoint(degree, x, y);
+    }
+  }
+
+  glEnd();
+}
+
+void updateItersOfFormulaCalc() {
+  int value = zoom * 10;
+  itersOfFormulaCalc = value < DEFAULT_ITERS_OF_FORUMA_CALC
+                           ? DEFAULT_ITERS_OF_FORUMA_CALC
+                           : value;
+}
+
+int calcProximityDegreeToCenter(FloatType x, FloatType y) {
+  Complex z, c;
+
+  z.real = x;
+  z.imag = y;
+  c.real = z.real;
+  c.imag = z.imag;
+
+  int n = 0;
+
+  for (; isAllowableComplexValue(z) && n < itersOfFormulaCalc; n++) {
+    calcIterableFormula(z, c);
+  }
+
+  return n;
+}
+
+/*
+ * Is the absolute value of complex number less than the constant value
+ */
+bool isAllowableComplexValue(Complex number) {
+  return (number.real * number.real + number.imag * number.imag) <
+         MAX_COMPLEX_NUMBER_ABSOLUTE_VALUE;
+}
+
+/*
+ * Iterable formula for calculating fractal:
+ * f(z) = z^2 + c
+ */
+void calcIterableFormula(Complex &z, Complex &c) {
+  Complex tempZ = z;
+  Complex tempC = c;
+
+  z.real = (tempZ.real * tempZ.real) - (tempZ.imag * tempZ.imag) + c.real;
+  z.imag = 2 * tempZ.real * tempZ.imag + c.imag;
+  c.real = tempC.real / 2 + z.real;
+  c.imag = tempC.imag / 2 + z.imag;
+}
+
+void renderPoint(int n, int x, int y) {
+  if (n >= itersOfFormulaCalc) {
+    return;
+  }
+
+  FloatType a = 35 * n % 255;
+  FloatType b = 30 * n % 255;
+  FloatType c = 25 * n % 255;
+
+  glColor3d(a / 255.0, b / 255.0, c / 255.0);
+  glVertex2d(x, y);
 }
